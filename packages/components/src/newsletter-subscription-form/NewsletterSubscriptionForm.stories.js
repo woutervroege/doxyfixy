376c16e228f19631c3a79fedb880d4cb9a7e8f61
Wouter Vroege <wouter@woutervroege.nl>
2023-09-05 22:37:56 +0200

    docs(components): naming

--- packages/components/src/newsletter-subscription-form/NewsletterSubscriptionForm.stories.js
+++ packages/components/src/newsletter-subscription-form/NewsletterSubscriptionForm.stories.js
@@ -2,7 +2,7 @@ import NewsletterSubscriptionForm from './NewsletterSubscriptionForm.svelte';
 
 // More on how to set up stories at: https://storybook.js.org/docs/svelte/writing-stories/introduction
 export default {
-  title: 'Example/NewsletterSubscriptionForm',
+  title: 'Example/Newsletter Subscription Form',
   component: NewsletterSubscriptionForm,
   tags: ['autodocs'],
   argTypes: {
--- packages/components/src/social-buttons/SocialButtons.stories.js
+++ packages/components/src/social-buttons/SocialButtons.stories.js
@@ -2,7 +2,7 @@ import SocialButtons from './SocialButtons.svelte';
 
 // More on how to set up stories at: https://storybook.js.org/docs/svelte/writing-stories/introduction
 export default {
-  title: 'Example/SocialButtons',
+  title: 'Example/Social Buttons',
   component: SocialButtons,
   tags: ['autodocs'],
   argTypes: {
