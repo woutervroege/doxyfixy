7fb7d5b630847a346bd67f76c7356095290efd93
Wouter Vroege <wouter@woutervroege.nl>
2023-09-05 22:36:40 +0200

    feat(components): add social buttons

---
+++ packages/components/src/social-buttons/SocialButtons.stories.js
@@ -0,0 +1,15 @@
+import SocialButtons from './SocialButtons.svelte';
+
+// More on how to set up stories at: https://storybook.js.org/docs/svelte/writing-stories/introduction
+export default {
+  title: 'Example/SocialButtons',
+  component: SocialButtons,
+  tags: ['autodocs'],
+  argTypes: {
+  },
+};
+
+export const Default = {
+  args: {
+  },
+};
