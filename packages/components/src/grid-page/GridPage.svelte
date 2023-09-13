2c977f525ba9d080a2628cb764a1c43205f305cf
Wouter Vroege <wouter@woutervroege.nl>
2023-09-13 06:27:21 +0200

    styling

---
+++ packages/components/src/grid-page/GridPage.svelte
@@ -0,0 +1,41 @@
+<script>
+    import PageHeader from '../page-header/PageHeader.svelte';
+    import Card from '../card/Card.svelte';
+    import './grid-page.css';
+
+
+   /**
+   * @type {string} title
+   */
+   export let title = '';
+
+    /**
+    * @type {string} intro
+    */
+    export let intro = '';
+
+    /**
+    * @type {array} tags
+    */
+    export let tags = [];
+    
+    /**
+    * @type {array} cards
+    */
+    export let cards = [];
+    
+</script>
+
+<div class="doxy-grid-page">
+
+    <PageHeader
+        title={title}
+        intro={intro}
+        tags={tags}
+    />
+
+    <div class="grid">
+        <slot/>
+    </div>
+
+</div>
