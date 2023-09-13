3c84c8a25ffcfba38e203fb07be711fd0408a111
Wouter Vroege <wouter@woutervroege.nl>
2023-09-13 06:08:36 +0200

    added page header

---
+++ packages/components/src/page-header/PageHeader.svelte
@@ -0,0 +1,44 @@
+<script>
+    import './page-header.css';
+    import Button from '../button/Button.svelte';
+
+   /**
+   * @type {string} title
+   */
+   export let title = '';
+
+   /**
+   * @type {string} intro
+   */
+   export let intro = '';
+
+   /**
+   * @type {array} tags
+   */
+   export let tags = [];
+
+   /**
+    * Has Tags
+   */
+    $: hasTags = tags.length > 0;
+
+</script>
+
+<section class="doxy-page-header">
+    <div>
+        {#if title}<h1>{title}</h1>{/if}
+        {#if intro}<p>{intro}</p>{/if}
+
+        {#if hasTags}
+        <nav>
+            {#each tags as tag}
+                <Button
+                    elevated
+                    label={tag}
+                    selectable
+                />
+            {/each}
+        </nav>
+        {/if}
+    </div>
+</section>
