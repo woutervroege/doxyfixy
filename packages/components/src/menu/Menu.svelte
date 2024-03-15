b9847a38e1cc73f0d38f78b4f55a56e4dbd69110
Wouter Vroege <wouter@woutervroege.nl>
2024-03-15 11:49:34 +0100

    adding english

--- packages/components/src/menu/Menu.svelte
+++ packages/components/src/menu/Menu.svelte
@@ -10,6 +10,11 @@
    export const label = '';
 
    /**
+   * @type {string} Link Prefix
+   */
+   export let prefix = '/';
+
+   /**
     * 
    */
     let open = false;
@@ -49,9 +54,9 @@
 <div class="{classNames.join(' ')}" role="menu">
 
     <div inert={!open} class="menuitems">
-        <a inert={!open} data-index="4" class="doxy-button" href="/films">Films</a>
-        <a inert={!open} data-index="2" class="doxy-button" href="/people">Mensen</a>
-        <a inert={!open} data-index="1" class="doxy-button" href="/about">Over ons</a>
+        <a inert={!open} data-index="4" class="doxy-button" href="{prefix}films">Films</a>
+        <a inert={!open} data-index="2" class="doxy-button" href="{prefix}people">People</a>
+        <a inert={!open} data-index="1" class="doxy-button" href="{prefix}about">About</a>
         <!-- <Switch client:visible/> -->
     </div>
 
